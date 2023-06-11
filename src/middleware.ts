3e6f0b9d23f303aa186d06dc004181ab3cad9971
Joshua Knauber <joshua.knauber@gmail.com>
2023-06-11 02:07:13 +0200

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/u/(.*)", "/a/(.*)", "/api/(.*)"],
+  publicRoutes: ["/", "/u/(.*)", "/a/(.*)"],
 });
 
 export const config = {
