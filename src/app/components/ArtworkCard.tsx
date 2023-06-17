c76856599e3146d2b9178ec68eb991ba3fbd0866
Joshua Knauber <joshua.knauber@gmail.com>
2023-06-17 17:12:57 +0200

    fix no thumbnail item

--- src/app/components/ArtworkCard.tsx
+++ src/app/components/ArtworkCard.tsx
@@ -33,47 +33,29 @@ const ArtworkCard = async ({
   return (
     <div
       className={
-        "group/artwork flex w-full flex-grow flex-col gap-2 transition-all md:hover:scale-[102%]"
+        "group/artwork relative flex w-full flex-grow flex-col gap-2 transition-all md:hover:scale-[102%]"
       }
       style={{
         aspectRatio: thumbnail ? thumbnail.width / thumbnail.height : 1,
       }}
     >
-      {thumbnail && (
-        <Link
-          href={minimal ? `/a/${artworkKey}?min=1` : `/a/${artworkKey}`}
-          className="relative w-full overflow-hidden"
-        >
-          <div className="absolute right-4 top-4 flex flex-row gap-2">
-            {wip && (
-              <span className="flex h-6 items-center rounded-full border border-purple-400 bg-purple-950 bg-opacity-70 px-2 text-sm leading-none text-purple-400">
-                WIP
-              </span>
-            )}
-            {feedback && (
-              <span className="flex h-6 w-7 items-center justify-center rounded-full border border-orange-400 bg-orange-950 bg-opacity-60 text-sm leading-none text-orange-400">
-                <ChatBubbleOvalLeftEllipsisIcon className="h-4 w-4" />
-              </span>
-            )}
-          </div>
-          {artist && (
-            <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.75)] opacity-0 transition-all md:hover:opacity-100">
-              <Link
-                href={`/user/${artist.username || ""}`}
-                className="absolute bottom-4 left-4 flex w-fit flex-row items-center gap-3"
-              >
-                <img
-                  src={artist.profileImageUrl || ""}
-                  className="h-7 w-7 rounded-full"
-                  style={{
-                    objectFit: "cover",
-                    aspectRatio: thumbnail.width / thumbnail.height,
-                  }}
-                />
-                <span className="leading-none">{artist.username || ""}</span>
-              </Link>
-            </div>
+      <Link
+        href={minimal ? `/a/${artworkKey}?min=1` : `/a/${artworkKey}`}
+        className="relative h-full w-full overflow-hidden"
+      >
+        <div className="absolute right-4 top-4 flex flex-row gap-2">
+          {wip && (
+            <span className="flex h-6 items-center rounded-full border border-purple-400 bg-purple-950 bg-opacity-70 px-2 text-sm leading-none text-purple-400">
+              WIP
+            </span>
           )}
+          {feedback && (
+            <span className="flex h-6 w-7 items-center justify-center rounded-full border border-orange-400 bg-orange-950 bg-opacity-60 text-sm leading-none text-orange-400">
+              <ChatBubbleOvalLeftEllipsisIcon className="h-4 w-4" />
+            </span>
+          )}
+        </div>
+        {thumbnail && (
           <img
             src={`https://uploadthing.com/f/${thumbnail.key}`}
             className="w-full"
@@ -81,7 +63,22 @@ const ArtworkCard = async ({
               aspectRatio: thumbnail ? thumbnail.width / thumbnail.height : 1,
             }}
           />
-        </Link>
+        )}
+      </Link>
+      {artist && (
+        <>
+          <div className="pointer-events-none absolute left-0 top-0 h-full w-full bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.75)] opacity-0 transition-all md:group-hover/artwork:opacity-100"></div>
+          <Link
+            href={`/user/${artist.username || ""}`}
+            className="absolute bottom-4 left-4 flex w-fit flex-row items-center gap-3 opacity-0 transition-all md:group-hover/artwork:opacity-100"
+          >
+            <img
+              src={artist.profileImageUrl || ""}
+              className="h-7 w-7 rounded-full"
+            />
+            <span className="leading-none">{artist.username || ""}</span>
+          </Link>
+        </>
       )}
     </div>
   );
