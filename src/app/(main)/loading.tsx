8c650b3ff02f373c22b6652f7ff51516eb8014e3
Joshua Knauber <joshua.knauber@gmail.com>
2023-06-20 18:18:24 +0200

    mobile nav

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -5,7 +5,6 @@ import ArtworkGrid from "../components/ArtworkGrid";
 export default function LoadingPage() {
   return (
     <>
-      <div className="sticky left-0 top-0 z-40 flex h-header w-full flex-row items-center justify-between border-b border-b-white border-opacity-10 bg-bg-500 px-4 md:px-8"></div>
       <div className="flex w-full px-4 py-4 md:px-8 md:py-8">
         <ArtworkGrid>
           <div className="h-[400px] w-full flex-grow bg-bg-600"></div>
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,11 +4,18 @@ import { useClerk } from "@clerk/nextjs";
 import Link from "next/link";
 import NotificationBell from "./NotificationBell";
 import { usePathname, useSearchParams } from "next/navigation";
-import { C } from "drizzle-orm/select.types.d-e43b2599";
 import { TwicImg } from "@twicpics/components/react";
+import {
+  ArrowUpTrayIcon,
+  Bars3BottomLeftIcon,
+  HomeIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const Header = () => {
   const { user } = useClerk();
+
+  const [showNav, setShowNav] = useState<boolean>(false);
 
   const path = usePathname();
   const searchParams = useSearchParams();
@@ -21,6 +28,12 @@ const Header = () => {
     <header className="sticky left-0 top-0 z-40 flex h-header w-full flex-row items-center justify-between border-b border-b-white border-opacity-10 bg-bg-500 px-4 md:px-8">
       <div className="flex flex-row items-center gap-8">
         <div className="flex flex-row items-center gap-4">
+          <button
+            className="flex h-8 w-8 flex-row items-center md:hidden"
+            onClick={() => setShowNav(!showNav)}
+          >
+            <Bars3BottomLeftIcon className="h-6 w-6 text-white" />
+          </button>
           <Link className="font-semibold leading-none" href="/">
             Duet
           </Link>
@@ -29,7 +42,7 @@ const Header = () => {
           </span>
         </div>
       </div>
-      <div className="flex flex-row items-center gap-8">
+      <div className="hidden flex-row items-center gap-8 md:flex">
         <Link
           href="/"
           className={`${path === "/" ? "opacity-100" : "opacity-50"}`}
@@ -52,9 +65,10 @@ const Header = () => {
         <div className="flex flex-row items-center gap-4">
           <Link
             href="/upload"
-            className="flex h-8 flex-row items-center rounded-full border border-blue-400 bg-blue-900 bg-opacity-0 px-5 text-sm font-medium leading-none text-blue-400 transition-all md:hover:bg-opacity-20"
+            className="flex h-8 flex-row items-center rounded-full border border-white bg-white bg-opacity-0 px-5 text-sm leading-none text-white transition-all md:hover:bg-opacity-5"
           >
-            Upload
+            <span className="hidden md:block">Upload</span>
+            <ArrowUpTrayIcon className="h-4 w-4 md:hidden" />
           </Link>
           <NotificationBell />
           <Link href={`/user/${user.username}`}>
@@ -72,6 +86,37 @@ const Header = () => {
           Sign In
         </Link>
       )}
+      <nav
+        className={`absolute left-0 top-header z-50 flex h-[calc(100dvh-4rem)] w-[100vw] origin-top-left flex-col gap-8 bg-bg-500 px-4 py-8 transition-all duration-75 md:hidden ${
+          showNav ? "scale-y-100" : "scale-y-0"
+        }`}
+      >
+        <Link
+          href="/"
+          onClick={() => setShowNav(false)}
+          className={`flex flex-row items-center gap-4 ${
+            path === "/" ? "opacity-100" : "opacity-50"
+          }`}
+        >
+          <HomeIcon className="h-5 w-5" />
+          Home
+        </Link>
+        <Link
+          onClick={() => setShowNav(false)}
+          href="/canvas"
+          className="flex flex-row items-center gap-2"
+        >
+          <div className="relative mx-2">
+            <div className="h-2 w-2 rounded-full bg-red-500"></div>
+            <div className="absolute left-0 top-0 h-2 w-2 animate-ping rounded-full bg-red-500"></div>
+          </div>
+          <span
+            className={`${path === "/canvas" ? "opacity-100" : "opacity-50"}`}
+          >
+            Canvas
+          </span>
+        </Link>
+      </nav>
     </header>
   );
 };
